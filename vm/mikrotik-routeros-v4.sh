9eb03f9a29395bd7e597308a855098ebb5b42d3d
barthdvs <102857359+barthdvs@users.noreply.github.com>
2023-01-15 18:41:25 +0100

    Update mikrotik-routeros-v4.sh

--- vm/mikrotik-routeros-v4.sh
+++ vm/mikrotik-routeros-v4.sh
@@ -210,7 +210,7 @@ msg_ok "Using ${CL}${BL}$STORAGE${CL} ${GN}for Storage Location."
 msg_ok "Virtual Machine ID is ${CL}${BL}$VMID${CL}."
 msg_info "Getting URL for Mikrotik RouterOS Disk Image"
 
-URL=https://download.mikrotik.com/routeros/7.6/install-image-7.6.zip
+URL=https://download.mikrotik.com/routeros/7.7/install-image-7.7.zip
 
 sleep 2
 msg_ok "${CL}${BL}${URL}${CL}"
